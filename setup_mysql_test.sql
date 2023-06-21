6be2b370080e7247150a649c33213e223528ef7a
Michae202 <mikewachirabb495@gmail.com>
2023-06-21 01:45:40 -0700

    AirBnB_clone_v2 root@c93771191cf1:/AirBnB_clone_v2# git commit -m AirBnB_clone_v2

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- a script that prepares a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant all privileges
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
