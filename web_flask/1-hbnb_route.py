fb80fe90d7c0ce9ddaebc279816023f0dd948126
Michae202 <mikewachirabb495@gmail.com>
2023-07-24 00:06:10 -0700

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-""" a script that starts flask web application """
+"""Start web application with two routings
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
-    """ a class with the name Hello """
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ a class with the name hbnb """
-    return 'HBNB!'
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
