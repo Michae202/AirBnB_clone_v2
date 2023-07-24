25c48e4b8693ee6842eca7ecbe0217303f355c65
Michae202 <mikewachirabb495@gmail.com>
2023-07-24 02:03:41 -0700

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
+"""A simple Flask server using our real HBNB data"""
 
-from models import storage
-from models.state import State
+
 from flask import Flask, render_template
-app = Flask(__name__)
+import models
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
+site = Flask(__name__)
+site.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
-    storage.close()
+@site.teardown_appcontext
+def closeStorageAfterRequest(error):
+    """Close and reload the storage engine between requests"""
+
+    models.storage.close()
+
+
+@site.route('/cities_by_states')
+def page_showStatesAndCities():
+    """List all the stored states and the cities within them"""
+
+    states = models.storage.all('State').values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    site.run(host='0.0.0.0', port=5000)
