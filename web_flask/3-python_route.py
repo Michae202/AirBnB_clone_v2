98a5d357fb631e54e186f6a6e431154cd2b3f354
Michae202 <mikewachirabb495@gmail.com>
2023-07-24 00:09:46 -0700

    a script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-""" a script that starts flask web application """
+"""Start web application with two routings
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
-    """ a class with the name Hello """
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ a class with the name hbnb """
-    return 'HBNB!'
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """ a class with the name c """
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    """ a class with the name python """
-    return 'python {}'.format(text.replace('_', ' '))
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
